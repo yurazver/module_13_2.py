c8ef2254dc6ab5dd76adf36e6a85aaa469f071aa
yurazver <yurazver0@gmail.com>
2024-10-18 19:17:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.utils import executor
+
+API_TOKEN = '*********'
+
+
+
+bot = Bot(token=API_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+    await message.reply('Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message_handler()
+async def all_messages(message: types.Message):
+    print('Введите команду /start, чтобы начать общение.')
+    await message.reply('Введите команду /start, чтобы начать общение.')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+
